2d41148d2534fc659e909fbfd561b617ab7af0c9
Zane Rice <zanetrice@gmail.com>
2021-09-12 06:43:27 -0400

    Simplify boolean logic in some validators.

--- src/validators/startThreadValidator.ts
+++ src/validators/startThreadValidator.ts
@@ -3,7 +3,6 @@ import { Message } from "discord.js";
 
 export class StartThreadValidator extends CommandValidator {
   validate(_args: string[], message: Message): boolean {
-    if (message.channel.isThread()) return false;
-    return true;
+    return !message.channel.isThread();
   }
 }
