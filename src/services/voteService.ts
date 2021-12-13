96b03bec69976a7e3686fa89c94a9017f47b3e23
Zane Rice <zanetrice@gmail.com>
2021-12-13 01:32:45 -0500

    Handle non-emojis better

--- src/services/voteService.ts
+++ src/services/voteService.ts
@@ -4,10 +4,14 @@ import { CommandService } from "./commandService";
 export class VoteService extends CommandService {
   async handleMessage(args: string[], message: Message): Promise<void> {
     const components = args.slice(1).map((arg, index) => {
-      const emoji = arg.includes(":") ? arg.substring(arg.lastIndexOf(":") + 1, arg.length - 1) : undefined;
+      const emoji =
+        arg.includes(":") && arg.includes("<") && arg.includes(">")
+          ? arg.substring(arg.lastIndexOf(":") + 1, arg.length - 1)
+          : "";
+      const label = emoji ? "" : arg.replace(/:/g, "");
       const button = new MessageButton()
-        .setCustomId(`vote:${index}:${message.id}:${emoji ? "" : arg}:${emoji ? emoji : ""}:0`)
-        .setLabel(`${emoji ? "" : arg} 0`)
+        .setCustomId(`vote:${index}:${message.id}:${label}:${emoji}:0`)
+        .setLabel(`${label} 0`)
         .setStyle("PRIMARY");
       if (emoji) button.setEmoji(emoji);
       return button;
