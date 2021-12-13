4f77b33e27c73c06ce289206f3f623d7eae3ddcf
Zane Rice <zanetrice@gmail.com>
2021-12-13 00:20:08 -0500

    Add test for when a second user votes

--- src/services/voteButtonInteractionService.test.ts
+++ src/services/voteButtonInteractionService.test.ts
@@ -87,6 +87,44 @@ describe("voteButtonInteractionService", () => {
           },
         ],
       });
+
+      const buttonInteractionThree: ButtonInteraction = {
+        customId: "voteButtonInteractionService:0:messageId:one:1",
+        update: jest.fn(),
+        member: {
+          user: {
+            id: "userId2",
+          },
+        },
+        message: {
+          components: [
+            {
+              components: [
+                {
+                  customId: "voteButtonInteractionService:0:messageId:one:1",
+                  label: "one 1",
+                },
+                {
+                  customId: "voteButtonInteractionService:1:messageId:two:0",
+                  label: "two 0",
+                },
+              ],
+            },
+          ],
+        } as unknown as Message,
+      } as unknown as ButtonInteraction;
+      voteButtonInteractionService.handleInteraction(buttonInteractionThree);
+      expect(buttonInteractionThree.update).toHaveBeenCalledWith({
+        components: [
+          {
+            components: [
+              { customId: "voteButtonInteractionService:0:messageId:one:2", label: "one 2", style: 1, type: 2 },
+              { customId: "voteButtonInteractionService:1:messageId:two:0", label: "two 0", style: 1, type: 2 },
+            ],
+            type: 1,
+          },
+        ],
+      });
     });
   });
 });
