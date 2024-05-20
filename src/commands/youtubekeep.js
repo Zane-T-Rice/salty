109aa4232e88ae2510583469b534be50f7b8f591
Zane Rice <zanetrice@gmail.com>
2024-05-20 16:53:42 -0400

    set description to shorter string

--- src/commands/youtubekeep.js
+++ src/commands/youtubekeep.js
@@ -3,9 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ytk")
-    .setDescription(
-      "Download a premium youtube to the YouTubeKeep folder on the jellyfin server under Zane's desk. Maybe Zane won't delete it if it's in there."
-    )
+    .setDescription("Download a premium youtube to the YouTubeKeep folder on the jellyfin server under Zane's desk.")
     .addStringOption((option) =>
       option.setName("args").setDescription("Space delimited urls that yt-dlp should try to download.")
     ),
