f2dc4f994ad76e14d3209097139dc68df1913f07
Zane Rice <zanetrice@gmail.com>
2025-04-24 01:09:21 -0400

    Adjust handleAutocomplete of updateServerService to use /hosts

--- src/services/updateServerService.ts
+++ src/services/updateServerService.ts
@@ -7,6 +7,12 @@ import { promisify } from "util";
 const exec = promisify(exec2);
 dotenv.config();
 
+type Host = {
+  id: string;
+  name: string;
+  url: string;
+};
+
 type Server = {
   id: string;
   applicationName: string;
@@ -62,15 +68,34 @@ export class UpdateServerService implements InteractionService {
         // If cache is more than 5 seconds old, refresh it.
         // This is just to prevent a user interacting with the autocomplete from triggering a bunch of API calls.
         const accessToken = await getAccessToken();
+
+        // First, get all the hosts
         const response = JSON.parse(
           (
             await exec(`curl --request GET \
-            --url ${process.env.SERVER_MANAGER_SERVICE_URL}/servers?isUpdatable=true \
+            --url ${process.env.SERVER_MANAGER_SERVICE_URL}/hosts \
             --header 'Authorization: Bearer ${accessToken}'`)
           ).stdout
         );
-        if (!(response instanceof Array)) throw new Error(); // The server returned an error response instead of an array of servers.
-        this.serversCache.servers = response;
+        if (!(response instanceof Array)) throw new Error(); // The server returned an error response instead of an array of hosts.
+        const hosts: Host[] = response;
+
+        // Now get all the servers on all the hosts
+        const responsePromises = hosts.map(async (host) => {
+          return JSON.parse(
+            (
+              await exec(`curl --request GET \
+            --url ${process.env.SERVER_MANAGER_SERVICE_URL}/hosts/${host.id}/servers?isUpdatable=true \
+            --header 'Authorization: Bearer ${accessToken}'`)
+            ).stdout
+          );
+        });
+        const responses = await Promise.all(responsePromises);
+        responses.forEach((response) => {
+          if (!(response instanceof Array)) throw new Error(); // The server returned an error response instead of an array of servers.
+        });
+        const servers: Server[] = (responses as Server[][]).reduce((a, b) => [...a, ...b]);
+        this.serversCache.servers = servers;
         this.serversCache.createdAt = Date.now();
       }
       const servers: Server[] = this.serversCache.servers;
