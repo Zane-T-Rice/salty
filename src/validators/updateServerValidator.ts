a99852761379d426a2bab112e2c171337d572fec
Zane Rice <zanetrice@gmail.com>
2025-04-24 03:25:45 -0400

    Update validators to account for hostId in server-manager-service interactions

--- src/validators/updateServerValidator.ts
+++ src/validators/updateServerValidator.ts
@@ -4,8 +4,12 @@ import { validate } from "uuid";
 
 export class UpdateServerValidator extends InteractionValidator {
   async validate(interaction: ChatInputCommandInteraction<CacheType>): Promise<boolean> {
-    const name = interaction.options.getString("name")?.trim(); // name here is actually the uuid of a server
-    const validName = validate(name);
+    const name = interaction.options
+      .getString("name")
+      ?.trim()
+      .split(" ")
+      .map((value) => value.trim()); // name here is actually the uuid of a host and server (space delimited)
+    const validName = Boolean(name && name.length === 2 && validate(name[0]) && validate(name[1]));
     const errorReply = [
       !validName ? "You must select an option from the list. (Or somehow type the uuid of the option manually)." : null,
     ]
